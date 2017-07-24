a96f872b170bbbb87d81df138ab7a8bcab6e1721
wentianyang <yqqq2015@gmail.com>
2017-07-24 22:32:29 +0800

    drawPath 画心形

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
@@ -2,28 +2,43 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
 
-public class Practice9DrawPathView extends View {
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP) public class Practice9DrawPathView extends View {
 
-    public Practice9DrawPathView(Context context) {
-        super(context);
-    }
+  private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
-    public Practice9DrawPathView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
+  private Path mPath = new Path();
 
-    public Practice9DrawPathView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
+  {
+    mPath.addArc(200, 200, 400, 400, -225, 225);
+    mPath.arcTo(400, 200, 600, 400, -180, 225, false);
+    mPath.lineTo(400,542);
+  }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+  public Practice9DrawPathView(Context context) {
+    super(context);
+  }
 
-//        练习内容：使用 canvas.drawPath() 方法画心形
-    }
+  public Practice9DrawPathView(Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public Practice9DrawPathView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP) @Override protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
+
+    //        练习内容：使用 canvas.drawPath() 方法画心形
+
+    canvas.drawPath(mPath, mPaint);
+  }
 }
